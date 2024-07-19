e6e6b13b19456256d19730267e64912a647a5524
AndrMaulana <andryymaulana2000@gmail.com>
2024-07-19 19:51:58 +0700

    Refactor : Add Field Createdat

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,10 +1,15 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
-	Username string `gorm:"unique;not null"`
-	Email    string `gorm:"unique;not null"`
-	Password string `gorm:"not null"`
+	Username  string    `json:"username" gorm:"unique;not null"`
+	Email     string    `json:"email" gorm:"unique;not null"`
+	Password  string    `json:"password" gorm:"not null"`
+	CreatedAt time.Time `json:"created_at" gorm:"not null"`
 }
