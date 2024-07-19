8d60a065543c346ddd2065890137ee1b03d6fb4a
AndrMaulana <andryymaulana2000@gmail.com>
2024-07-20 00:29:55 +0700

    Feat : Create Feature Task Models

--- internal/models/task.go
+++ internal/models/task.go
@@ -6,7 +6,9 @@ type Task struct {
 	gorm.Model
 	Title       string `gorm:"not null"`
 	Description string
-	Status      string
+	Status      string `gorm:"not null;default:'pending'"`
 	ProjectID   uint
+	Project     Project `gorm:"foreignKey:ProjectID"`
 	AssigneeID  uint
+	Assignee    User `gorm:"foreignKey:AssigneeID"`
 }
