d6d9329274994fa97316a4d7614291666195e0f8
AndrMaulana <andryymaulana2000@gmail.com>
2024-07-20 00:27:30 +0700

    Feat : Create Feature Projects Models

--- internal/models/project.go
+++ internal/models/project.go
@@ -6,5 +6,7 @@ type Project struct {
 	gorm.Model
 	Name        string `gorm:"not null"`
 	Description string
-	UserID      uint
+	OwnerID     uint
+	Owner       User   `gorm:"foreignKey:OwnerID"`
+	Users       []User `gorm:"many2many:project_users;"`
 }
